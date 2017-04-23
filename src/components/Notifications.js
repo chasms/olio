c89a55962ad51b410baf0929902f3f46301561b1
Alexandra Roth <alroth8@gmail.com>
2017-04-23 18:01:03 -0400

    some cleanup

--- src/components/Notifications.js
+++
@@ -1,7 +0,0 @@
-// std library imports
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-
-// app imports
-import { addNotification, removeNotification } from '../actions/notifications'
--- src/reducers/NotificationsReducer.js
+++
@@ -1,8 +0,0 @@
-export default function Notifications(state = [], action){
-  switch (action.type) {
-    case 'GET_DRAWERS':
-      return action.payload
-    default:
-      return state
-  }
-}
