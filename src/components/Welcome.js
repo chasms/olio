c192cc013b139741d8a2204156f0088533d92511
Alexandra Roth <alroth8@gmail.com>
2017-04-24 13:27:37 -0400

    welcome modal stuffs

---
+++ src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Welcome extends React.Component{
+
+render() {
+
+  return (
+    <div className="welcome">
+      <h3>Welcome To Olio!</h3>
+          <p>click to add content from the drawers</p>
+          <p>check out the keyboard in the nav for keyboard shortcuts</p>
+          <h4>Have fun!</h4>
+    </div>
+    )
+  }
+}
