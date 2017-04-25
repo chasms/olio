932f60f910e48e0626b80632f3f2b60aa67aedef
Alexandra Roth <alroth8@gmail.com>
2017-04-25 10:34:48 -0400

    messages

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -10,7 +10,9 @@ export default class Welcome extends React.Component{
         <p>~ click to add content from the drawers ~</p>
         <p>~ once it's on the page, drag and resize ~</p>
         <p>~ check out the keyboard in the nav for shortcuts ~</p>
+        <p>~ click again on an object to get rid of the border ~</p>
         <p>~ login to save and manage your creations ~</p>
+
         <h4>~ Have fun! ~</h4>
       </div>
     )
