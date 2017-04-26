4aa669a3295e0f0cd8ccc5549fd4bbd63e96e132
chasms <chasms@chas.ms>
2017-04-26 15:26:12 -0400

    centered notifications

--- src/actions/consts.js
+++ src/actions/consts.js
@@ -1,48 +1,48 @@
 export const signupError = (error) => ({
   title: 'Error Signing Up',
   message: error,
-  position: 'tl',
+  position: 'tc',
   autoDismiss: 4
 })
 
 export const loginError = {
   title: 'Sorry!',
   message: 'Your username and password didn\'t match',
-  position: 'tl',
+  position: 'tc',
   autoDismiss: 5
 }
 
 export const logoutAlert = {
   title: 'Logout!',
   message: 'You have logged out',
-  position: 'tl',
+  position: 'tc',
   autoDismiss: 3
 }
 
 export const signupSuccess = {
   title: 'Signup Successful',
   message: 'You have successfully signed up for Olio!',
-  position: 'tl',
+  position: 'tc',
   autoDismiss: 3
 }
 
 export const loginSuccess = {
   title: 'Log In Successful',
   message: 'You have logged in to Olio!',
-  position: 'tl',
+  position: 'tc',
   autoDismiss: 3
 }
 
 export const saveSuccess = {
   title: 'Creation Saved!',
   message: 'You have saved your creation!',
-  position: 'tl',
+  position: 'tc',
   autoDismiss: 3
 }
 
 export const deleteSuccess = (id) => ({
   title: 'Deleted Creation!',
   message: 'You have deleted creation #' + id,
-  position: 'tl',
+  position: 'tc',
   autoDismiss: 3
 })
